1916d99cfb5739f28ccaf2c14f18639430589731
Philipp Bandow <43735306+philband@users.noreply.github.com>
2024-06-17 11:44:02 +0200

    Migrate permission assignment to MgGraph

--- config/deploy/StrongAuthDashboardAssignPerms.ps1
+++ config/deploy/StrongAuthDashboardAssignPerms.ps1
@@ -1,13 +1,11 @@
-$miObject = "object-id-of-your-MI"
 $permissionsToAdd = "User.Read.All", "UserAuthenticationMethod.Read.All", "Group.Read.All", "AuditLog.Read.All"
+# static Graph AppId, do not change
+$graphAppId = "00000003-0000-0000-c000-000000000000"
 
-#Graph
-$appId = "00000003-0000-0000-c000-000000000000"
-
-Connect-AzureAD
-$app = Get-AzureADServicePrincipal -Filter "AppId eq '$appId'"
-foreach ($permission in $permissionsToAdd)
-{
-   $role = $app.AppRoles | where Value -Like $permission | Select-Object -First 1
-   New-AzureADServiceAppRoleAssignment -Id $role.Id -ObjectId $miObject -PrincipalId $miObject -ResourceId $app.ObjectId
+Connect-MgGraph
+$msi = Get-MgServicePrincipal -Filter "DisplayName eq 'Import-StrongAuthDashboadData'"
+$app = Get-MgServicePrincipal -Filter "AppId eq '$graphAppId'"
+foreach ($permission in $permissionsToAdd) {
+   $role = $app.AppRoles | Where-Object Value -Like $permission | Select-Object -First 1
+   New-MgServicePrincipalAppRoleAssignment -AppRoleId $role.Id -ServicePrincipalId $msi.Id -PrincipalId $msi.Id -ResourceId $app.Id
 }
